05b1c341d5a3d543b83aea25d63fb162bc3f5384
Farid <69624747+FaridArriaga@users.noreply.github.com>
2021-03-07 19:04:18 -0600

    6. Step 6, first commit of new branch generated after yb and master merge

--- fDualGit.py
+++ fDualGit.py
@@ -6,3 +6,7 @@ print("Step 2, first yB branch commit")
 
 # third commit, step 3
 st3 = "Step 3, 2nd yB branch commit"
+
+# step 6, commit after yb and master merge, new branch; yBSt6
+st6 = 6
+print(st6**3)
