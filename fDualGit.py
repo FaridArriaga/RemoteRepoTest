04ef8fbbc08ca7ff78f58c575f94f59956188406
Farid <69624747+FaridArriaga@users.noreply.github.com>
2021-03-07 19:20:46 -0600

    7. Step 7, new branch generated with tag vStep 5 advance, and first commit of this branchs -> new operations added

--- fDualGit.py
+++ fDualGit.py
@@ -6,3 +6,7 @@ print("Step 2, first yB branch commit")
 
 # third commit, step 3
 st3 = "Step 3, 2nd yB branch commit"
+
+#step 7, new branch generated with the advance of the tag vStep5 
+st7 = 7
+print(type(21/7))
