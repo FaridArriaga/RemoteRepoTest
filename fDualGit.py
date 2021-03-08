7df069aa8cb01e8887babc9b3b497bcb4d606925
Farid <69624747+FaridArriaga@users.noreply.github.com>
2021-03-07 19:39:54 -0600

    8. Step 8, list added

--- fDualGit.py
+++ fDualGit.py
@@ -10,3 +10,9 @@ st3 = "Step 3, 2nd yB branch commit"
 #step 7, new branch generated with the advance of the tag vStep5 
 st7 = 7
 print(type(21/7))
+
+#step 8 
+st7 = 7
+listNs = [1,2,3,4,5,6,7,8]
+if st7 in listNs:
+    print("st7 appears in listNs")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
 st7 = 7
-print(type(21/7))
+listNs = [1,2,3,4,5,6,7,8]
+if st7 in listNs:
+    print("st7 appear in listNs")
